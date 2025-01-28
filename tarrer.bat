b7262d429788ac0541ff3c90313c1e0df129e498
invalidusernamecalled <puneetbapna@gmail.com>
2025-01-28 23:43:18 +0530

    Support Windows drag & drop

--- tarrer.bat
+++ tarrer.bat
@@ -1,5 +1,5 @@
 @echo off
-
+if exist "%*" set "inpt_fle=%*"&goto process
 set exclude_pattern=
 if "%~1"==""   (goto printhelp)
 if "%~1"=="/?" (goto printhelp)
@@ -10,16 +10,15 @@ if exist "%~1\*" set /a file=0
 if %file%==0 (echo argument is a directory) else (echo argument is a file)
 if %file%==1 for /f "delims=" %%i in ('dir /b "%~1"') do set "inpt_fle=%%~i"
 if %file%==0 set "inpt_fle=%~1"
+:process
 call :setonlyname "%inpt_fle%"
 :regen
 set /a RAND=%RANDOM%*9999/32767
 if exist "%~1%RAND%.tar.gz" goto regen
-echo:tar -r -f "%fl_nm%%RAND%.tar.gz" "%inpt_fle%"
 if "%exclude_pattern%" NEQ "" (tar -r -f "%fl_nm%%RAND%.tar.gz" --exclude %exclude_pattern% "%inpt_fle%") else (tar -r -f "%fl_nm%%RAND%.tar.gz" "%inpt_fle%")
 
 echo ERRORCODE:%errorlevel%
 if %errorlevel%==0 (echo Output File: "%fl_nm%%RAND%.tar.gz")
-
 goto :eof
 :setonlyname
 for /f "delims=" %%i in (%1) do set "fl_nm=%~n1"
@@ -33,5 +32,4 @@ echo:
 
 echo archive name is auto-generated. does not add to existing archive.
 echo:
-
 
