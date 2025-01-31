cbabd09f9ef54dbe1ad768cf5784e513620b30b6
invalidusernamecalled <puneetbapna@gmail.com>
2025-01-31 15:06:35 +0530

    bug wonderlust

--- tarrer.bat
+++ tarrer.bat
@@ -1,5 +1,8 @@
-@echo off
-REM if exist "%*" set "inpt_fle=%*"&goto process
+@echo off & setlocal enabledelayedexpansion
+set inpt_fle=
+set inpt_dir=
+if defined archive-extension for %%a in (.rar .gz .bz2 .xz .lzma) do if /i "%archive-extension%"=="%%a" (echo:Using Ext:%archive-extension%) else (set archive-extension=.rar)
+if not defined archive-extension echo Using default extension:.rar&set archive-extension=.rar
 set exclude_pattern=
 if "%~1"==""   (goto printhelp)
 if "%~1"=="/?" (goto printhelp)
@@ -8,20 +11,27 @@ if not exist "%~1" echo:File not exist & goto :eof
 set /a file=1
 if exist "%~1\*" set /a file=0
 if %file%==0 (echo argument is a directory) else (echo argument is a file)
-if %file%==1 for /f "delims=" %%i in ('dir /b "%~1"') do set "inpt_fle=%%~i"
-if %file%==0 set "inpt_fle=%~1"
+call :setonlydir "%~1"
+goto process
 :process
-call :setonlyname "%inpt_fle%"
+call :setonlyname "%~1"
+echo FULL TARGET NAME: %inpt_dir%\%fl_nm%
+echo tar -r -f "%fl_nm_only%%RAND%%archive-extension%" "%~1" -C "%inpt_dir%"
 :regen
 set /a RAND=%RANDOM%*9999/32767
-if exist "%~1%RAND%.tar.gz" goto regen
-if "%exclude_pattern%" NEQ "" (tar -r -f "%fl_nm%%RAND%.tar.gz" --exclude %exclude_pattern% "%inpt_fle%") else (tar -r -f "%fl_nm%%RAND%.tar.gz" "%inpt_fle%")
+if exist "%fl_nm_only%%RAND%%archive-extension%" goto regen
+if "%exclude_pattern%" NEQ "" (tar -r -f "%fl_nm_only%%RAND%%archive-extension%" --exclude %exclude_pattern% "%~1"  -C "%inpt_dir%" ) else (tar -r -f "%fl_nm_only%%RAND%%archive-extension%" "%~1" -C "%inpt_dir%")
 set /a program_error_level=%errorlevel%
-if %program_error_level%==0 (if exist "%fl_nm%%RAND%.tar.gz" (echo Output File: "%fl_nm%%RAND%.tar.gz"&call :seterror 0) else (call :seterror 1)) else (call :seterror 1)
+if %program_error_level%==0 (if exist "%fl_nm_only%%RAND%%archive-extension%" (echo Output File: "%fl_nm_only%%RAND%%archive-extension%"&call :seterror 0) else (call :seterror 1)) else (call :seterror 1)
 echo:tar[%program_error_level%]*******"%~nx0"[%errorlevel%]
 goto :eof
 :setonlyname
-for /f "delims=" %%i in (%1) do set "fl_nm=%~n1"
+for /f "delims=" %%i in (%1) do set "fl_nm=%~nx1"&set "fl_nm_only=%~n1"
+goto :eof
+:setonlydir
+if exist %1 echo %1 exists. 
+for /f "delims=" %%i in ('dir %1') do set "inpt_dir=%~dp1"&echo:"%~dp1":will change to it before processing.&goto thatsit
+:thatsit
 goto :eof
 :seterror
 exit /b %1
@@ -34,4 +44,3 @@ echo:
 
 echo archive name is auto-generated. does not add to existing archive.
 echo:
-
