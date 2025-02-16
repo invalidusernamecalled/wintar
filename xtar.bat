22d2ae08f43db858172cdadd773f31d46e17e8ed
invalidusernamecalled <puneetbapna@gmail.com>
2025-02-17 03:39:11 +0530

    Add files via upload

--- xtar.bat
+++ xtar.bat
@@ -6,9 +6,20 @@ set "fl_nm_only=%~n1"
 set "archive-choice=%~x1"
 set /a extensionfound=0
 if defined archive-choice for %%a in (.tar .tar.gz .tar.bz2 .tar.xz .tar.lzma) do if /i "%archive-choice%"=="%%a" (set /a extensionfound=1)
-if %extensionfound%==1 goto regen
+if %extensionfound%==1 goto next
 choice /c yn /m "does not seem to be valid extension. do u want to still try (default=n)" /d n /t 2
 if %errorlevel%==2 goto :eof
+:next
+set /a foldcount=0
+if "%~2"=="/s" for /f "delims=" %%i in ('tar -t -f "%~1" ^| findstr /r "[/]$" ^| find /c /v ""') do set /a foldcount=%%i
+if "%~2"=="/s" if %foldcount%==1 set check=&set /a start=1 & goto foldset
+goto regen
+:foldset
+for /f "tokens=%start% delims=/" %%i in ('tar -t -f "%~1" ^| findstr /r "[/]$" ') do set "dirofconcern=%%i"&set "check=%%i"
+if "%check%"=="" goto regen
+set check=
+set /a start+=1
+if %start% GTR 254 echo:Fail too many long path & goto :eof
 :regen
 set /a maxxtri+=1
 set /a RAND=%RANDOM%*9999/32767
@@ -22,6 +33,7 @@ if exist "%fl_nm_only%%archive-choice%" tar -x -f  "%fl_nm_only%%archive-choice%
 echo|set/p=tar[%errorlevel%]
 del  "%fl_nm_only%%archive-choice%"
 if exist "..\%fl_nm_only%%RAND%" popd
+if "%~2"=="/s" if %foldcount%==1 if exist "%fl_nm_only%%RAND%" pushd "%fl_nm_only%%RAND%" & move "%dirofconcern%\*" .\ 1>NUL & rd "%dirofconcern%" & popd
 goto :eof
 :printhelpmenu
 echo:Tar extracter v.1
